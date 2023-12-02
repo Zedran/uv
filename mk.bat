53bd2573f9390c72ddecb2582ab6efecb75bac2b
Zedran <17592740+Zedran@users.noreply.github.com>
2023-12-02 13:59:09 +0100

    add go mod tidy to mk scripts

--- mk.bat
+++ mk.bat
@@ -1,3 +1,4 @@
 @ECHO OFF
 
+go mod tidy
 go build -trimpath -ldflags "-s -w" -o build/uv.exe ./src
--- mk.sh
+++ mk.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
 
+go mod tidy
 go build -trimpath -ldflags "-s -w" -o build/uv ./src
